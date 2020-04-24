68c9a39874be8128abbf95b93caf63820b8f6c23
josh <josh.buchanan00@gmail.com>
2020-04-24 16:14:57 -0600

    added tkinter

--- index.py
+++ index.py
@@ -12,12 +12,16 @@ conn = psycopg2.connect(dbcredentials)
 conn.set_session(autocommit=True)
 
 cursor = conn.cursor()
+input_id = 1
 
-query = '''select * from user_info'''
+query = f'''select * from user_info where id = {input_id}'''
 
 cursor.execute(query)
 
-print(cursor.fetchall())
+user_info = cursor.fetchall()
+user_first_name = user_info[0][1].strip()
+user_last_name = user_info[0][2].strip()
+welcome = "Welcome, " + user_first_name + " " + user_last_name
 
 cursor.close()
 
---
+++ view.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from index import *
+
+root = Tk()
+
+myLabel = Label(root, text = welcome)
+
+myLabel.pack()
+
+root.mainloop()
