46235d4fc7e57d2196614821942b7a963a72b53e
josh <josh.buchanan00@gmail.com>
2020-04-27 17:35:49 -0600

    added button

--- view.py
+++ view.py
@@ -1,10 +1,25 @@
 from tkinter import *
-from index import *
+from index import welcome
+
+boolean = True
 
 root = Tk()
 
-myLabel = Label(root, text = welcome)
+def whatToShow():
+    if(boolean):
+        return "Hello friend"
+    else:
+        return "Hello enemy"
+def clickedButton():
+    boolean = False
+    myLabel = Label(root, text=whatToShow())
+    print(boolean)
+    myLabel.pack()
 
-myLabel.pack()
+button = Button(root, text="Click Here", command=clickedButton)
+    
+
+button.pack()
+
 
 root.mainloop()
