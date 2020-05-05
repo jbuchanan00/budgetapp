62fa1adea76736807eaca054a8d3469443fd0a3a
josh <josh.buchanan00@gmail.com>
2020-05-04 20:20:00 -0600

    info input onto root

--- index.py
+++ index.py
@@ -8,6 +8,8 @@ from login import *
 
 conn = psycopg2.connect(dbcredentials)
 tk = Tk()
+tk.geometry("1200x800")
+#create login screen
 loginScreen = Toplevel()
 loginScreen.title("login")
 
@@ -15,21 +17,28 @@ conn.set_session(autocommit=True)
 
 
 
-user_info = []
+user_info = ["no one"]
 
-def login_submit(id):
+def login_submit():
     cursor = conn.cursor()
+    id = loginEntry.get()
     query = f'''select * from user_info where id = {id}'''
     cursor.execute(query)
-    print(cursor.fetchall())
+    user_info = cursor.fetchall()
+    print(user_info[0])
     cursor.close()
+    #kills login screen without killing the main program
     loginScreen.withdraw()
+    displayName = Label(tk, text=user_info[0])
+    displayName.pack()
 
-loginButton = Button(loginScreen, text="hope", command=lambda:login_submit(1))
+loginButton = Button(loginScreen, text="hope", command=lambda:login_submit())
 loginEntry = Entry(loginScreen, width=60)
+
 
 loginButton.grid(row=1, column=0)
 loginEntry.grid(row=0, column=0)
+
 
 loginScreen.mainloop()
 tk.mainloop()
