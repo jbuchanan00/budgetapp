771b734c2b72ce8b062fc28fd94d6b1e79b771ae
josh <josh.buchanan00@gmail.com>
2020-05-08 17:23:43 -0600

    added radio buttons

--- index.py
+++ index.py
@@ -15,11 +15,20 @@ loginScreen.title("login")
 
 conn.set_session(autocommit=True)
 
+#Global Variables
+userFirstName = ""
+userLastName = ""
+id = 0
+inSourceFreq = 0
+
 
 
 user_info = ["no one"]
 
 def login_submit():
+    global userFirstName
+    global userLastName
+    global id
     cursor = conn.cursor()
     id = loginEntry.get()
     query = f'''select * from user_info where id = {id}'''
@@ -29,15 +38,66 @@ def login_submit():
     cursor.close()
     #kills login screen without killing the main program
     loginScreen.withdraw()
-    displayName = Label(tk, text=user_info[0])
-    displayName.pack()
+    userFirstName = user_info[0][1].strip()
+    userLastName = user_info[0][2].strip()
+    print(userFirstName, userLastName)
+    userFirstNameLabel = Label(tk, text=userFirstName)
+    userLastNameLabel = Label(tk, text=userLastName)
+    userFirstNameLabel.grid(row=0, column=0)
+    userLastNameLabel.grid(row=0, column=1)
 
-loginButton = Button(loginScreen, text="hope", command=lambda:login_submit())
+
+
+
+
+
+
+
+#Labels
+userFirstNameLabel = Label(tk, text=userFirstName)
+userLastNameLabel = Label(tk, text=userLastName)
+loginLabel = Label(loginScreen, text="Login")
+incomeSourceLabel = Label(tk, text="Income Source")
+incomeSourceFreqLabel = Label(tk, text="Frequency of Income")
+
+
+#Buttons
+loginButton = Button(loginScreen, text="Login", command=lambda:login_submit())
+
+
+
+#Inputs
 loginEntry = Entry(loginScreen, width=60)
+incomeSource = Entry(tk, width=30)
+
+#RadioButton
+incomeSourceOneTime = Radiobutton(tk, text="One Time", variable=inSourceFreq, value=1)
+incomeSourceBiWeek = Radiobutton(tk, text="Bi-Weekly", variable=inSourceFreq, value=2)
+incomeSourceMonth = Radiobutton(tk, text="Monthly", variable=inSourceFreq, value=3)
+incomeSourceSemiAnn = Radiobutton(tk, text="Semi-Annually", variable=inSourceFreq, value=4)
+incomeSourceYear = Radiobutton(tk, text="Yearly", variable=inSourceFreq, value=5)
 
 
-loginButton.grid(row=1, column=0)
-loginEntry.grid(row=0, column=0)
+#Layout
+#Labels
+loginLabel.grid(row=0, column=0)
+userFirstNameLabel.grid(row=0, column=0)
+userLastNameLabel.grid(row=0, column=1)
+incomeSourceLabel.grid(row=1, column=0)
+
+#Buttons
+loginButton.grid(row=2, column=0)
+
+#Inputs
+loginEntry.grid(row=1, column=0)
+incomeSource.grid(row=2, column=0)
+
+#RadioButtons
+incomeSourceOneTime.grid(row=3, column=1)
+incomeSourceBiWeek.grid(row=3, column=2)
+incomeSourceMonth.grid(row=3, column=3)
+incomeSourceSemiAnn.grid(row=3, column=4)
+incomeSourceYear.grid(row=3, column=5)
 
 
 loginScreen.mainloop()
