97ef7985711b62fbb6c1e5194d40367fea78f79b
josh <josh.buchanan00@gmail.com>
2020-05-18 19:03:45 -0600

    button passing info

--- index.py
+++ index.py
@@ -57,76 +57,93 @@ def submitInClick():
 
 def getInfoClick():
     global id
+    global incomeRow
     cursor = conn.cursor()
     query = f'select * from income where user_id = {id}'
     cursor.execute(query)
     info = cursor.fetchall()
-    j = 6
+    incomeRow = 6
     for i in info:
-        
         if i[2] == 2:
             frequency = "Bi-Weekly"
         elif i[2] == 3:
             frequency = "Monthly"
-        infoLabel = Label(tk, text=i[1].strip())
+        source = i[1].strip()
+        infoButton = Button(tk, text=i[1].strip(), command=lambda: incomeItem(i))
         frequencyLabel = Label(tk, text=frequency)
-        infoLabel.grid(row=j, column=0)
-        frequencyLabel.grid(row=j, column=1)
-        j = j + 1
-        
+        infoButton.bind("<Button-1>", infoButtonClick)
+        infoButton.grid(row=incomeRow, column=0)
+        frequencyLabel.grid(row=incomeRow, column=1)
+        incomeRow = incomeRow + 1
+    
+def infoButtonClick(event):
+    print(event.widget)
+
+def incomeItem(src):
+    global incomeRow
+    print(src)
+    incomeItem = Entry(tk, width=30)
+    incomeItemDate = Entry(tk, width=30)
+    incomeItem.grid(row=incomeRow, column=2, columnspan=2)
+    incomeItemDate.grid(row=incomeRow, column=4, columnspan=2)
 
 
 
+def incomeScreenClick():
+    #Income screen Labels
+    userFirstNameLabel = Label(tk, text=userFirstName)
+    userLastNameLabel = Label(tk, text=userLastName)
+    incomeSourceLabel = Label(tk, text="Income Source")
+    #Income screen Buttons
+    submitIn = Button(tk, text="Submit", command=submitInClick)
+    incomeInfo = Button(tk, text="Income info", command=getInfoClick)
+    #Income screen Entries 
+    incomeSource = Entry(tk, width=30)
+    #Income screen Radio Buttons 
+    incomeSourceOneTime = Radiobutton(tk, text="One Time", variable=inSourceFreq, value=1)
+    incomeSourceBiWeek = Radiobutton(tk, text="Bi-Weekly", variable=inSourceFreq, value=2)
+    incomeSourceMonth = Radiobutton(tk, text="Monthly", variable=inSourceFreq, value=3)
+    incomeSourceSemiAnn = Radiobutton(tk, text="Semi-Annually", variable=inSourceFreq, value=4)
+    incomeSourceYear = Radiobutton(tk, text="Yearly", variable=inSourceFreq, value=5)
+    #Income screen grid placement
+    #Labels
+    userFirstNameLabel.grid(row=0, column=0)
+    userLastNameLabel.grid(row=0, column=1)
+    incomeSourceLabel.grid(row=1, column=1, columnspan=2)
+    #Buttons
+    submitIn.grid(row=4, column=2)
+    incomeInfo.grid(row=5, column=0)
+    #Entries
+    incomeSource.grid(row=2, column=1, columnspan=2)
+    #Radio Buttons
+    incomeSourceOneTime.grid(row=3, column=0)
+    incomeSourceBiWeek.grid(row=3, column=1)
+    incomeSourceMonth.grid(row=3, column=2)
+    incomeSourceSemiAnn.grid(row=3, column=3)
+    incomeSourceYear.grid(row=3, column=4) 
 
-
+#Login/MainPage
 #Labels
-userFirstNameLabel = Label(tk, text=userFirstName)
-userLastNameLabel = Label(tk, text=userLastName)
 loginLabel = Label(loginScreen, text="Login")
-incomeSourceLabel = Label(tk, text="Income Source")
-incomeSourceFreqLabel = Label(tk, text="Frequency of Income")
-
 
 #Buttons
 loginButton = Button(loginScreen, text="Login", command=lambda:login_submit())
-submitIn = Button(tk, text="Submit", command=submitInClick)
-incomeInfo = Button(tk, text="Income info", command=getInfoClick)
+incomeScreenButton = Button(tk, text="Income", command=incomeScreenClick)
 
 
 #Inputs
 loginEntry = Entry(loginScreen, width=60)
-incomeSource = Entry(tk, width=30)
-
-#RadioButton
-incomeSourceOneTime = Radiobutton(tk, text="One Time", variable=inSourceFreq, value=1)
-incomeSourceBiWeek = Radiobutton(tk, text="Bi-Weekly", variable=inSourceFreq, value=2)
-incomeSourceMonth = Radiobutton(tk, text="Monthly", variable=inSourceFreq, value=3)
-incomeSourceSemiAnn = Radiobutton(tk, text="Semi-Annually", variable=inSourceFreq, value=4)
-incomeSourceYear = Radiobutton(tk, text="Yearly", variable=inSourceFreq, value=5)
-
 
 #Layout
 #Labels
 loginLabel.grid(row=0, column=0)
-userFirstNameLabel.grid(row=0, column=0)
-userLastNameLabel.grid(row=0, column=1)
-incomeSourceLabel.grid(row=1, column=1, columnspan=2)
 
 #Buttons
 loginButton.grid(row=2, column=0)
-submitIn.grid(row=4, column=2)
-incomeInfo.grid(row=5, column=0)
+incomeScreenButton.grid(row=0, column=3)
 
 #Inputs
 loginEntry.grid(row=1, column=0)
-incomeSource.grid(row=2, column=1, columnspan=2)
-
-#RadioButtons
-incomeSourceOneTime.grid(row=3, column=0)
-incomeSourceBiWeek.grid(row=3, column=1)
-incomeSourceMonth.grid(row=3, column=2)
-incomeSourceSemiAnn.grid(row=3, column=3)
-incomeSourceYear.grid(row=3, column=4)
 
 loginScreen.mainloop()
 tk.mainloop()
